3f84c62b781ed9dd9fbe872d5732c3f70c8567a0
Resch Said <25457206+Resch-Said@users.noreply.github.com>
2024-04-17 03:40:18 +0200

    Add login and store cookies to activate games

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os.path
+
 import requests
 import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 def get_steam_sessionid():
@@ -10,17 +16,43 @@ def get_steam_sessionid():
     return sessionid
 
 
+def get_cookies_steam_login_secure():
+    if os.path.isfile('cookies.txt'):
+        with open('cookies.txt', 'r') as f:
+            cookies = json.loads(f.read())
+            return cookies[1]['value']
+    return None
+
+
+# Get steam identity by logging in with selenium
 def get_steam_login_secure():
-    pass
+    url = f"https://store.steampowered.com/login/?redir=%3Fsnr%3D1_60_4__global-header&redir_ssl=1&snr=1_4_4__global-header"
+
+    if get_cookies_steam_login_secure() is not None:
+        return get_cookies_steam_login_secure()
+
+    driver = webdriver.Chrome()
+    driver.get(url)
+    try:
+        element = WebDriverWait(driver, 600).until(
+            EC.presence_of_element_located((By.ID, "account_pulldown"))  # Wait until user is logged in
+        )
+    finally:
+        cookies = driver.get_cookies()
+        with open('cookies.txt', 'w') as f:
+            f.write(json.dumps(cookies))
+        driver.quit()
+
+    if get_cookies_steam_login_secure() is not None:
+        return get_cookies_steam_login_secure()
 
 
-# TODO: Get steam login secure
 # TODO: Get the true appid of the game. For example, the appid of "Muck" is 1625450, but the post request needs the appid 577069
 def activate_free_steam_game(appid=1782210):
     sessionid = get_steam_sessionid()
 
     url = f"https://store.steampowered.com/freelicense/addfreelicense/577069"
-    steam_login_secure = ""
+    steam_login_secure = get_steam_login_secure()
     headers = {
         "accept-language": "de,de-DE;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
         "cookie": f"sessionid={sessionid}; steamLoginSecure={steam_login_secure}",
