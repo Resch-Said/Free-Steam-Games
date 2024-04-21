7ffffc0e692c910100506bb51c6c4c5a351c2629
Resch Said <25457206+Resch-Said@users.noreply.github.com>
2024-04-21 19:13:10 +0200

    create main Method to update database and redeem games

---
+++ main.py
@@ -0,0 +1,20 @@
+from time import sleep
+from database import Database
+from steam import Steam
+
+
+def main():
+    while True:
+        break_time = 8 * 60 * 60  # hours
+
+        Database.main()
+        Steam.main()
+        while break_time > 0:
+            break_time -= 1
+            print(f"Taking a break for {break_time} seconds ({round((
+                  break_time / 60 / 60), 2)} hours)", end="\r")
+            sleep(1)
+
+
+if __name__ == "__main__":
+    main()
