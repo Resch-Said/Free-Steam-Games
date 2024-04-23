dffca84ed20a7642cec5276f1453df3b9e89de10
Resch Said <25457206+Resch-Said@users.noreply.github.com>
2024-04-23 04:26:59 +0200

    only use Betterpath if necessary

--- src/database.py
+++ src/database.py
@@ -12,8 +12,9 @@ from webdriver import Webdriver
 
 
 class Database:
+    BetterPath.create_path("../database")
 
-    con = sqlite3.connect(BetterPath.get_absolute_path("../database") + "/steam.db")
+    con = sqlite3.connect("../database" + "/steam.db")
     cur = con.cursor()
     app_detail_url = "https://store.steampowered.com/api/appdetails?appids="
 
