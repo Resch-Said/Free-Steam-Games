0699639dd247abab689fd7fafa7fa0570d568f35
Resch Said <25457206+Resch-Said@users.noreply.github.com>
2024-05-06 21:24:33 +0200

    disable log file for now

--- src/logger.py
+++ src/logger.py
@@ -4,10 +4,12 @@ from datetime import datetime
 class Logger:
 
     @classmethod
-    def write_log(cls, message, print_message=True):
+    def write_log(cls, message, print_message=True, log_file=False):
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
         if print_message:
             print(message)
-        with open("../log.txt", "a") as file:
-            file.write(current_time + ": " + message + "\n")
+
+        if log_file:
+            with open("../log.txt", "a") as file:
+                file.write(current_time + ": " + message + "\n")
