22e541f1df4df43e72ee23f73f58287c6b558680
Resch Said <25457206+Resch-Said@users.noreply.github.com>
2024-05-07 04:47:30 +0200

    fix unicodeEncodeError

--- src/logger.py
+++ src/logger.py
@@ -5,11 +5,13 @@ class Logger:
 
     @classmethod
     def write_log(cls, message, print_message=True, log_file=False):
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
         if print_message:
-            print(message)
+            try:
+                print(message)
+            except UnicodeEncodeError:
+                print(message.encode("utf-8"))
 
         if log_file:
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             with open("../log.txt", "a") as file:
                 file.write(current_time + ": " + message + "\n")
