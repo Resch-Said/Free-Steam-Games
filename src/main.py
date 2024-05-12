7edeaea0d91bb5feca38e0b15fb809840211e35d
Resch Said <25457206+Resch-Said@users.noreply.github.com>
2024-05-12 08:35:38 +0200

    replace threading with async

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import threading
 from datetime import datetime, timedelta
 from time import sleep
+import asyncio
 
 from database import Database
 from exit_listener import ExitListener
@@ -26,36 +27,31 @@ def break_time(break_time_hours):
         sleep(1)
 
 
-def add_steam_games():
+async def add_steam_games():
     while not ExitListener.get_exit_flag():
         Steam.main()
         break_time(break_time_hours=8)
 
 
-def update_database():
+async def update_database():
     while not ExitListener.get_exit_flag():
         Database.main()
         break_time(break_time_hours=0.1)
 
 
-def main():
+async def main():
     ExitListener.start()
     Logger.write_log(f"Current Version: {Settings.get_software_version()}")
 
     if not Steam.check_if_user_is_logged_in():
         Steam.open_steam_login_page()
 
-    database_thread = threading.Thread(target=update_database)
-    steam_thread = threading.Thread(target=add_steam_games)
+    tasks = [add_steam_games(), update_database()]
 
-    database_thread.start()
-    steam_thread.start()
-
-    database_thread.join()
-    steam_thread.join()
+    await asyncio.gather(*tasks)
 
     Logger.write_log("Done!")
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
